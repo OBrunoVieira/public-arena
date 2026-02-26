af591d143d7c8ec2ee863a445f867c384df02df4
Bruno Vieira <bruno.vieira@lynkco.com>
2026-02-26 18:22:21 -0300

    update checksum

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
     ],
     dependencies: [
         // KMAuth depends on GoogleSignIn natives
-        .package(url: "https://github.com/google/GoogleSignIn-iOS", from: "7.0.0")
+        .package(url: "https://github.com/google/GoogleSignIn-iOS", "7.0.0"..<"10.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "commentsystem",
             url: "https://github.com/OBrunoVieira/public-arena/releases/download/1.0.1-alpha06/commentsystem.xcframework.zip",
-            checksum: "5a6e544e07fdaa29b0f13ebad953f5cae2a2231108ee4c914923970714e711ba"
+            checksum: "a3640028ca409fed4dd66e00e2f327d083b0fda466c50f1b50415753c57d1559"
         ),
         .target(
             name: "CommentSystemTarget",
