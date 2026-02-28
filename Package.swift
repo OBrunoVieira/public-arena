5286e1ecfe2ddba043dfe2bfb30e659aa84ec85f
Bruno Vieira <bruno.vieira@lynkco.com>
2026-02-28 18:16:02 -0300

    update package.swift

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "commentsystem",
-            url: "https://github.com/OBrunoVieira/public-arena/releases/download/1.0.1-alpha062/commentsystem.xcframework.zip",
-            checksum: "a3640028ca409fed4dd66e00e2f327d083b0fda466c50f1b50415753c57d1559"
+            url: "https://github.com/OBrunoVieira/public-arena/releases/download/1.0.1-alpha063/commentsystem.xcframework.zip",
+            checksum: "cc201f615f78a8b376238dccdd7678e7b2f8f04ee43d73ed60c9af8b95c517c3"
         ),
         .target(
             name: "CommentSystemTarget",
