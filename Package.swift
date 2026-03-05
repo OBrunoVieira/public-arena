327aeeabe4db3c9336a2e335a92467389bd7588e
Bruno Vieira <brunovieiracrz@gmail.com>
2026-03-05 22:29:21 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "public-arena",
+    name: "CommentSystemSDK",
     platforms: [
         .iOS(.v14)
     ],
@@ -14,22 +14,26 @@ let package = Package(
     ],
     dependencies: [
         // KMAuth depends on GoogleSignIn natives
-        .package(url: "https://github.com/google/GoogleSignIn-iOS", "7.0.0"..<"10.0.0")
+        .package(url: "https://github.com/google/GoogleSignIn-iOS", "7.0.0"..<"10.0.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "commentsystem",
-            url: "https://github.com/OBrunoVieira/public-arena/releases/download/1.0.1-alpha063/commentsystem.xcframework.zip",
-            checksum: "cc201f615f78a8b376238dccdd7678e7b2f8f04ee43d73ed60c9af8b95c517c3"
+            url: "https://github.com/OBrunoVieira/public-arena/releases/download/1.0.1-alpha064/commentsystem.xcframework.zip",
+            checksum: "b783ee116d4a3fd8e242156bf45912117b98106e41f1fa6ca497f34f8ad1f438"
         ),
         .target(
             name: "CommentSystemTarget",
             dependencies: [
                 .target(name: "commentsystem"),
-                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS")
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCore", package: "firebase-ios-sdk")
             ],
             linkerSettings: [
-                .unsafeFlags(["-ObjC"])
+                .linkedLibrary("c++"),
+                .unsafeFlags(["-ObjC", "-all_load"])
             ]
         )
     ]
