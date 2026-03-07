51291ad7339a2978431e0098a416eeb357dc2000
Bruno Vieira <brunovieiracrz@gmail.com>
2026-03-07 04:15:24 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,35 +6,34 @@ let package = Package(
     platforms: [
         .iOS(.v14)
     ],
-    // products: [
-    //     .library(
-    //         name: "CommentSystemSDK",
-    //         targets: ["CommentSystemTarget"]
-    //     ),
-    // ],
+    products: [
+        .library(
+            name: "CommentSystemSDK",
+            targets: ["CommentSystemTarget"]
+        ),
+    ],
     dependencies: [
         // KMAuth depends on GoogleSignIn natives
-        .package(url: "https://github.com/google/GoogleSignIn-iOS", "7.0.0"..<"10.0.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "12.0.0")
+        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "7.1.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "commentsystem",
-            url: "https://github.com/OBrunoVieira/public-arena/releases/download/1.0.1-alpha065/commentsystem.xcframework.zip",
-            checksum: "b783ee116d4a3fd8e242156bf45912117b98106e41f1fa6ca497f34f8ad1f438"
+            url: "https://github.com/OBrunoVieira/public-arena/releases/download/1.0.1-alpha066/commentsystem.xcframework.zip",
+            checksum: "16b795566eefd836003b0b8e9a1ac05d2399df1c1942ce750bc840016fa223db"
         ),
-        // .target(
-        //     name: "CommentSystemTarget",
-        //     dependencies: [
-        //         .target(name: "commentsystem"),
-        //         .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
-        //         .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
-        //         .product(name: "FirebaseCore", package: "firebase-ios-sdk")
-        //     ],
-        //     linkerSettings: [
-        //         .linkedLibrary("c++"),
-        //         .unsafeFlags(["-ObjC", "-all_load"])
-        //     ]
-        // )
+        .target(
+            name: "CommentSystemTarget",
+            dependencies: [
+                .target(name: "commentsystem"),
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk")
+            ],
+            linkerSettings: [
+                .linkedLibrary("c++"),
+                .unsafeFlags(["-ObjC", "-all_load"])
+            ]
+        )
     ]
 )
